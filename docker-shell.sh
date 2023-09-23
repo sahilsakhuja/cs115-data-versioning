2c9803cbc511d7c9068fa273922f59f63b6ffb91
sahilsakhuja <sahilsakhuja85@gmail.com>
2023-09-23 17:01:22 +0000

    dataset_updates

--- docker-shell.sh
+++ docker-shell.sh
@@ -4,15 +4,16 @@ set -e
 
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCS_BUCKET_NAME="mushroom-app-data-demo"
-export GCP_PROJECT="ac215-project"
-export GCP_ZONE="us-central1-a"
+export GCS_BUCKET_NAME="mushroom-app-data-demo-sahil"
+export GCP_PROJECT="cs115-advanced-ds"
+export GCP_ZONE="eu"
 
 # Create the network if we don't have it yet
 docker network inspect data-versioning-network >/dev/null 2>&1 || docker network create data-versioning-network
 
 # Build the image based on the Dockerfile
-docker build -t data-version-cli --platform=linux/arm64/v8 -f Dockerfile .
+# docker build -t data-version-cli --platform=linux/arm64/v8 -f Dockerfile .
+docker build -t data-version-cli -f Dockerfile .
 
 # Run Container
 docker run --rm --name data-version-cli -ti \
